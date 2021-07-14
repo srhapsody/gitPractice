20bd40c8befa4e296ffac43bfc43d33d47ba25d9
yohanlim-labtop <21600855@handong.edu>
2021-07-14 15:31:57 +0900

    final

--- calculator.cpp
+++ calculator.cpp
@@ -2,28 +2,6 @@
 
 using namespace std;
 
-int main(){
-    int a,b;
-    cout<<"first number : ";
-    cin >> a;
-
-    cout<<"second number : ";
-    cin >> b;
-
-    cout<<"what operator do you want ? (+ - * /)"<<endl;
-
-    char op;
-
-    switch(op){
-        case '+' : addition(a,b);break;
-        case '-' : subtraction(a,b);break;
-        case '*' : multiplication(a,b);break;
-        case '/' : division(a,b);break;
-    }
-
-    return 0;
-}
-
 void multiplication(int a, int b) {
 	cout << a * b << endl;
 }
@@ -39,3 +17,28 @@ void division(int a, int b){
 void subtraction(int a, int b){
     cout<<a-b<<endl;
 }
+
+
+int main(){
+    int a,b;
+    cout<<"first number : ";
+    cin >> a;
+
+    cout<<"second number : ";
+    cin >> b;
+
+    cout<<"what operator do you want ? (+ - * /)";
+    
+
+    char op;
+    cin>>op;
+    switch(op){
+        case '+' : addition(a,b);break;
+        case '-' : subtraction(a,b);break;
+        case '*' : multiplication(a,b);break;
+        case '/' : division(a,b);break;
+    }
+
+    return 0;
+}
+
